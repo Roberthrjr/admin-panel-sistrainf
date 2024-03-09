25d7ce506d59fa5323b5e0ba1387b04a5f911c05
Roberth Rios <tauroroberth00@gmail.com>
2024-03-08 19:41:29 -0500

    Implementacion de consultas a nivel id

--- src/views/Consultas.jsx
+++ src/views/Consultas.jsx
@@ -1,42 +1,22 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useData from "../hooks/useAxios";
 
 export default function Consultas() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [result, setResult] = useState(null);
+  const {
+    data: result,
+    loading,
+    error,
+    refetch,
+  } = useData(
+    `${import.meta.env.VITE_ENDPOINT_BASE}/CarneConductor/${searchTerm}`
+  );
 
-  // Función para manejar la búsqueda
   const handleSearch = () => {
-    // Realizar la lógica de búsqueda aquí, por ejemplo, hacer una solicitud al servidor
-    // Aquí puedes poner la lógica de búsqueda según el DNI o el código de carné
-    // Supongamos que tienes una función `buscarConductor` que realiza la búsqueda
-    const conductorEncontrado = buscarConductor(searchTerm);
-    setResult(conductorEncontrado);
+    refetch();
   };
 
-  // Función simulada para buscar conductor
-  const buscarConductor = (term) => {
-    // Aquí simulas la búsqueda basada en el término (DNI o código de carné)
-    // Por ejemplo, puedes buscar en una lista de conductores o hacer una solicitud al servidor
-    // Devuelve los datos del conductor encontrado o null si no se encontró
-    // Esto es solo un ejemplo simulado, debes adaptarlo a tu lógica de backend
-    const conductorEncontrado = conductores.find(
-      (conductor) => conductor.dni === term || conductor.codigo_carne === term
-    );
-    return conductorEncontrado;
-  };
-
-  // Datos simulados de conductores
-  const conductores = [
-    {
-      nombre: "Juan",
-      apellido: "Perez",
-      dni: "12345678",
-      codigo_carne: "CARN-123",
-      // Otros datos del conductor...
-    },
-    // Otros conductores...
-  ];
   return (
     <div className="max-w-lg mx-auto mt-8">
       <div className="bg-white rounded-lg shadow-lg overflow-hidden">
@@ -59,23 +39,34 @@ export default function Consultas() {
               Buscar
             </button>
           </div>
+          {loading && <p>Cargando...</p>}
+          {error && <p>Error: {error.message}</p>}
           {result && (
-            <div className="border-t border-gray-200 p-4">
+            <div
+              className={`border-t border-gray-200 p-4 ${
+                result.estado_carne === "Habilitado"
+                  ? "text-green-600"
+                  : "text-red-600"
+              }`}
+            >
               <p>
-                <span className="font-bold">Nombre:</span> {result.nombre}{" "}
-                {result.apellido}
+                <span className="font-bold">Nombre:</span>{" "}
+                {result.nombre_conductor} {result.apellido_conductor}
               </p>
               <p>
-                <span className="font-bold">DNI:</span> {result.dni}
+                <span className="font-bold">DNI:</span> {result.dni_conductor}
               </p>
               <p>
                 <span className="font-bold">Código de Carné:</span>{" "}
                 {result.codigo_carne}
               </p>
+              <p>
+                <span className="font-bold">Estado:</span> {result.estado_carne}
+              </p>
               {/* Otros datos del conductor... */}
             </div>
           )}
-          {!result && searchTerm && (
+          {!result && searchTerm && !loading && (
             <p className="text-red-600">
               No se encontró ningún conductor con el DNI o código de carné
               ingresado.
